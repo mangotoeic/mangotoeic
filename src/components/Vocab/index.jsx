7d3eba520331597135d01a65160d995cec6f553e
parkgeunhong <super1947@naver.com>
2020-10-27 13:09:52 +0900

    마스텨병합2

--- src/components/Vocab/index.jsx
+++ src/components/Vocab/index.jsx
@@ -1,2 +1,1 @@
 export {default as Vocab} from './Vocab.jsx'
-
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as AddReview} from "./AddReview"
-export {default as ReviewInput} from "./ReviewInput"
-export {default as ReviewList} from "./ReviewList"
-export {default as ShowReview} from "./ShowReview"
-
