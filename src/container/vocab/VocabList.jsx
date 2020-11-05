17d3603df74e43fc0e206efeee7c448750eca528
Hyunah <hkwon9@masonlive.gmu.edu>
2020-11-05 20:15:55 +0900

    11/5 8:15

--- src/container/vocab/VocabList.jsx
+++ src/container/vocab/VocabList.jsx
@@ -20,12 +20,14 @@ import {
   } from 'reactstrap';
 const VocabList = () => {
   // const [vocabId, setVocabId] = useState(1)
+  const [id, setId] = useState(sessionStorage.getItem('sessionUser'))
+
   const [vocabs, setVocabs] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchVocabs = async () => {
+    const fetchVocabs = async id => {
       try {
         // 요청이 시작 할 때에는 error 와 tests 를 초기화하고
         setError(null);
@@ -33,22 +35,22 @@ const VocabList = () => {
         // loading 상태를 true 로 바꿉니다.
         setLoading(true);
         const response = await axios.get(
-          'http://127.0.0.1:8080/api/vocabs'
+          `http://127.0.0.1:8080/api/vocab/${id}`
         );
         setVocabs(response.data);
-        console.log(response.data) // 데이터는 response.data 안에 들어있습니다.
+        // console.log(response.data) // 데이터는 response.data 안에 들어있습니다.
       } catch (e) {
         setError(e);
       }
       setLoading(false);
     };
 
-    fetchVocabs();
+    fetchVocabs(id);
   }, []);
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!vocabs) return null;
-
+console.log(Object.values(vocabs))
 return<>
 <VocabPage>
 <Container>
@@ -56,12 +58,17 @@ return<>
 <Row className="show-grid" key={index}>
 <Col lg="2">
     <p className="vocab mt-3">
-      {vocab.vocabId}
+      {Object.keys(vocab)}
     </p>
   </Col>
   <Col lg="10">
     <p className="meaning mt-3">
-      {vocab.vocab}
+      {(Object.values(vocab)).map((meaning,index)=>(
+        // <Row className="show-grid" key={index}>
+        
+       <Col>{meaning}</Col> 
+      
+      ))}
     </p>
   </Col>
 </Row>
