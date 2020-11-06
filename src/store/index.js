76efa9645ce7798c1ade4d655a48ffe7c40c7e27
parkgeunhong <super1947@naver.com>
2020-11-06 18:17:08 +0900

    20201106 18:17

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import {createStore} from 'redux'
+ 
+import reviewReducer from './reviewReducer'
+export {default as testReducer } from './testReducer'
+export {addOdapQidAction,addUserInfoAction, timeReducer ,returnCurrentTime,userInfoFromTestReducer,
+     isActiveAction, timerToggleReducer, initOdapQidAction,addResultAction,diagnosisTestReducer,increaseNumAction,initNumAction, testgenReducer} from './testReducer'
+export {diagnosisReducer, addUserDiagnosisAction} from './diagnosisReducer'
+
+
+
+const reviewstore = createStore(reviewReducer)
+
+export default reviewstore
